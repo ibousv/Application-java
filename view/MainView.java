7d6d6237dff19832a5a310bd1312a18691fa3f08
ibousv <devilsog2.0@gmail.com>
2024-04-21 10:54:43 +0000

    panel testing

--- view/MainView.java
+++ view/MainView.java
@@ -4,10 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainView {
-  /*
-   * JLabel label = new JLabel();
-   * JPanel panel = new JPanel();
-   */
+
+  /* JLabel label = new JLabel(); */
+  JPanel panel = new JPanel();
   JFrame frame = new JFrame();
 
   public static void main(String[] args) {
@@ -19,11 +18,16 @@ public class MainView {
      * frame.add(label);
      * frame.add(panel);
      */
+
+    panel.setBackground(Color.cyan);
+    panel.setBounds(100, 100, 300, 300);
+    panel.setVisible(true);
+    frame.add(panel);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setTitle("AGS");
     frame.setSize(600, 400);
     frame.setVisible(true);
-    
+
   }
 
 }
