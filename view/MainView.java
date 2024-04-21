ab68501498c5630cff7e7fd505e28f38ec7278db
ibousv <devilsog2.0@gmail.com>
2024-04-21 10:31:46 +0000

    init Main view

---
+++ view/MainView.java
@@ -0,0 +1,29 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainView {
+  /*
+   * JLabel label = new JLabel();
+   * JPanel panel = new JPanel();
+   */
+  JFrame frame = new JFrame();
+
+  public static void main(String[] args) {
+    MainView main = new MainView();
+  }
+
+  public MainView() {
+    /*
+     * frame.add(label);
+     * frame.add(panel);
+     */
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setTitle("AGS");
+    frame.setSize(600, 400);
+    frame.setVisible(true);
+    
+  }
+
+}
