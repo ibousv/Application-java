b1b524ace834835f1b6a74502ca9c6d7d2d20983
ibousv <devilsog2.0@gmail.com>
2024-04-24 02:05:02 +0000

    update

--- model/Notes.java
+++ model/Notes.java
@@ -1,26 +1,13 @@
 package model;
 
-import java.util.List;
-
 public class Notes {
 	private int valeur;
 
 	private Cours cours;
 
-	private Etudiant etudiant;
-
-	private List<Administrateur> administrateur;
-
-	public void save() {
-
-	}
-
-	public void modNotes() {
-
-	}
-
-	public void displayNotes() {
-
+	public Notes(int valeur, Cours cours) {
+		this.valeur = valeur;
+		this.cours = cours;
 	}
 
 }
