ee62b588e1e8e86ec6d91671f6bcfc3165a8c3b6
ibousv <devilsog2.0@gmail.com>
2024-04-24 23:57:15 +0000

    refacto

--- model/Etudiant.java
+++ model/Etudiant.java
@@ -1,11 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Etudiant extends Utilisateur {
 
-    
+    private ArrayList<Notes> mesNotes = new ArrayList<Notes>();
+
     public Etudiant(String nom, String prenom, String login, String password) {
         super(nom, prenom, login, password);
 
     }
 
+    void ListerNotes() {
+        for (Notes notes : mesNotes) {
+            System.out.println(notes.getValeur());
+        }
+    }
 }
