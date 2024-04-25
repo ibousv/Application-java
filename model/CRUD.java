d1b91fe2ee79975a2fbbf5edd5b59a22a31d0a18
ibousv <devilsog2.0@gmail.com>
2024-04-25 03:45:06 +0000

    refacto

--- model/CRUD.java
+++ model/CRUD.java
@@ -6,62 +6,26 @@ package model;
  * Interface CRUD
  * Container of all methods ->
  * insert() display() delete() update()
+ * 
  * @author ib
  * @version v1.0.0
  */
 interface CRUD {
-	/**
-	 * 
-	 * @param u
-	 */
-	void insert(Utilisateur u);
-
-	/**
-	 * 
-	 * @param c
-	 */
 	void insert(Cours c);
 
-	/**
-	 * 
-	 * @param n
-	 */
-	void insert(Notes n);
-
-	/**
-	 * 
-	 * @param e
-	 */
-	void display(Etudiant e);
-	
-	/**
-	 * 
-	 * @param c
-	 */
-	void display(Cours c);
-
-	/**
-	 * 
-	 * @param n
-	 */
-	void display(Notes n);
-
-	/**
-	 * 
-	 * @param e
-	 */
-	void update(Etudiant e);
-
-	
 	void update(Cours c);
 
-	//
-	void update(Notes n);
-
-	//
-	void delete(Etudiant e);
-
-	//
 	void delete(Cours c);
 
+	void insert(Etudiant e);
+
+	void update(Etudiant e);
+
+	void delete(Etudiant e);
+
+	void insert(Notes n);
+
+	void update(Notes n);
+
+	void display(Notes n);
 }
