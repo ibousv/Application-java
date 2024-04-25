44222126031ce0ad26026c47d6edf3b3c61348e4
ibousv <devilsog2.0@gmail.com>
2024-04-25 00:13:46 +0000

    fix

--- model/Administrateur.java
+++ model/Administrateur.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.ArrayList;
-
 public class Administrateur extends Utilisateur {
 
 	Base base = new Base();
@@ -9,5 +7,5 @@ public class Administrateur extends Utilisateur {
 	public Administrateur(String nom, String prenom, String login, String password) {
 		super(nom, prenom, login, password);
 
-	}
+	}	
 }
