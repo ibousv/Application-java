155c8ca4cb331ad64bf00878f695c40bd04a096f
ibousv <devilsog2.0@gmail.com>
2024-04-25 00:06:45 +0000

    refacto

--- model/Administrateur.java
+++ model/Administrateur.java
@@ -1,21 +1,13 @@
 package model;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Administrateur extends Utilisateur {
 
-	public Administrateur(String nom, String prenom, long id) {
-		super(nom, prenom, id);
-	}
-
-	private Notes lesNotes;
-	private Cours lesCours;
-	private Etudiant lesEtudiants;
-	private List<Cours> cours;
-	private List<Notes> notes;
-	private List<Etudiant> etudiant;
-
 	Base base = new Base();
 
+	public Administrateur(String nom, String prenom, String login, String password) {
+		super(nom, prenom, login, password);
 
+	}
 }
