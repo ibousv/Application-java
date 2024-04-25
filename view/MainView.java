098c3c21cc3c17ebb1b1718fcedd9daaa01b4e44
ibousv <devilsog2.0@gmail.com>
2024-04-25 15:08:08 +0000

    update

--- view/MainView.java
+++ view/MainView.java
@@ -5,29 +5,4 @@ import java.awt.*;
 
 public class MainView {
 
-  /* JLabel label = new JLabel(); */
-  JPanel panel = new JPanel();
-  JFrame frame = new JFrame();
-
-  public static void main(String[] args) {
-    MainView main = new MainView();
-  }
-
-  public MainView() {
-    /*
-     * frame.add(label);
-     * frame.add(panel);
-     */
-
-    panel.setBackground(Color.cyan);
-    panel.setBounds(100, 100, 300, 300);
-    panel.setVisible(true);
-    frame.add(panel);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setTitle("AGS");
-    frame.setSize(600, 400);
-    frame.setVisible(true);
-
-  }
-
 }
