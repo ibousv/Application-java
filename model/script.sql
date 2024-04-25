609fe3536a79bc0f2ce4ffd1f260388413a5576f
ibousv <devilsog2.0@gmail.com>
2024-04-25 03:33:46 +0000

    fix

--- model/script.sql
+++ model/script.sql
@@ -1,25 +1,26 @@
-CREATE TABLE cours ( id_cours int PRIMARY KEY ,
-                    nom_cours varchar(20),
-                    coeff int 
+CREATE DATABASE ags;
+USE ags;
+
+
+
+CREATE TABLE cours ( nom varchar(20) PRIMARY KEY,
+                     enseignant varchar(100)
                     );
 
-CREATE TABLE notes( id_notes int PRIMARY KEY,
-                   valeur_notes int,
-                   notes_cours int,
-                   FOREIGN KEY(notes_cours) REFERENCES cours(id_cours)
-                   );
-
-CREATE TABLE etudiants( id_etudiants int PRIMARY KEY,
-                       nom_etudiants varchar(50),
-                       motpasse_etudiants varchar(50),
-                       mes_notes int,
-                       FOREIGN KEY(mes_notes) REFERENCES notes(id_notes)
-                    );
-CREATE TABLE administrateurs(id_etudiants int PRIMARY KEY,
-                             nom_administrateurs varchar(50),
-                             motpasse_administrateurs varchar(50),
-                             les_notes int,
-                             les_cours int,
-                             FOREIGN KEY(les_notes) REFERENCES notes(id_notes),
-                         	 FOREIGN KEY(les_cours) REFERENCES cours(id_cours)
+CREATE TABLE administrateurs(login  varchar(20) PRIMARY KEY,
+                             nom varchar(50),
+                             motpasse varchar(50)
                              );
+
+CREATE TABLE etudiants( login varchar(20) PRIMARY KEY,
+                       nom varchar(50),
+                       prenom varchar(50),
+                       motpasse varchar(50)
+                    );
+
+CREATE TABLE notes( valeur int,
+                   nom_cours varchar(50),
+                   login_etudiant varchar(20),
+                   FOREIGN KEY(nom_cours) REFERENCES cours(nom),
+                   FOREIGN KEY(login_etudiant) REFERENCES etudiants(login)
+                   );
