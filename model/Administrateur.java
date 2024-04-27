35e0ccfda5108c2cfba83363acdd05cd5c589860
ibousv <devilsog2.0@gmail.com>
2024-04-27 19:15:04 +0000

    testing

--- model/Administrateur.java
+++ model/Administrateur.java
@@ -1,5 +1,12 @@
 package model;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Document;
 public class Administrateur extends Utilisateur {
 
 	Base base = new Base();
@@ -7,5 +14,26 @@ public class Administrateur extends Utilisateur {
 	public Administrateur(String nom, String prenom, String login, String password) {
 		super(nom, prenom, login, password);
 
+	}
+
+	public void pdf_QRCode_gen(String login, String pass) {
+		Etudiant et = new Etudiant(null, null, login, pass);
+		try {
+			Document pdf = new Document();
+			PdfWriter writer = PdfWriter.getInstance(pdf, new FileOutputStream(login + ".pdf"));
+			pdf.open();
+			pdf.add(new Paragraph("A Hello World PDF document."));
+			pdf.close();
+			writer.close();
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
