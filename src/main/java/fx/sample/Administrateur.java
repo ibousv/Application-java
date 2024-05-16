d8f3a32b31cbc0e98f56c0f4e93f29f73ec7869b
ib <132789212+ibousv@users.noreply.github.com>
2024-05-16 07:25:13 +0000

    fix

--- src/main/java/fx/sample/Administrateur.java
+++ src/main/java/fx/sample/Administrateur.java
@@ -8,23 +8,5 @@ public class Administrateur extends Utilisateur{
         super(nom, prenom, login, password);
 
     }
-    public String affiche(Etudiant e) {
-        base.connection();
-        String sql = "select nom_cours, valeur from notes where login_etudiant = ?";
-        String info = "";
-        try {
-            base.pre = base.con.prepareStatement(sql);
-            base.pre.setString(1, e.getLogin());
-            base.re = base.pre.executeQuery();
-            while (base.re.next()) {
-                info += base.re.getString("nom_cours") + "   " + base.re.getInt("valeur") + "\n";
-            }
-            return info;
-        } catch (Exception ex) {
-            System.out.println("Une erreur est survenue :" + ex.getMessage());
-        }
-        return null;
-    }
-
 
 }
